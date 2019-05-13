1c9bf77720f245a21b34b8a4bad9501598e261e8
dilaragebes <48089660+dilaragebes@users.noreply.github.com>
2019-05-13 23:51:55 +0300

    Update crime_record.sql

--- login/crime_record.sql
+++ login/crime_record.sql
@@ -1,1 +1,1 @@
-INSERT INTO `crime_record` VALUES ('batuhan', 'uzumcu', 123456, '', 'izmir', 'kod yazmak');
+INSERT INTO `registration` VALUES ('batuhan', 'uzumcu', 123456, '25101997', 'izmir', 'kod yazmak','crime report örneği','2');
