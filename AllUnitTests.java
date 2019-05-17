a5ff6871f8ea7ae06c6467a5cbcd3e77842eb4e1
gizemhayuk <46076160+gizemhayuk@users.noreply.github.com>
2019-05-17 10:44:05 +0300

    Add files via upload

--- AllUnitTests.java
+++ AllUnitTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ Login.class, CriminalRecord.class })
+@SuiteClasses({ Login.class, CriminalRecord.class, EditingRecord.class, AdminPage.class })
 public class AllUnitTests {
 
 }
